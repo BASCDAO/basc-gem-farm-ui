2c466504761fa8b20ecb79d24cc05b94a9ed92f6
Exxempt <46376436+Exxempt@users.noreply.github.com>
2023-04-29 13:44:13 -0500

    Fix PDA import

--- hooks/useGemFarmStaking.ts
+++ hooks/useGemFarmStaking.ts
@@ -1,4 +1,4 @@
-import { findFarmerPDA } from "@gemfix/dist/gem-farm/gem-farm.pda"
+import { findFarmerPDA } from "../lib/gem-farm/common/extras"
 import { SignerWalletAdapter } from "@solana/wallet-adapter-base"
 import { useEffect, useState, useCallback } from "react"
 import { useAnchorWallet, useConnection } from "@solana/wallet-adapter-react"
